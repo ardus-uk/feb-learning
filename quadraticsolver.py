ea1d0153380e19aedc7ae35058de965405f72378
ardus-uk <peter@ardus.co.uk>
2013-02-27 10:14:56 +0000

    Update quadraticsolver.py

--- quadraticsolver.py
+++ quadraticsolver.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# solves quadratic equation
+# Finds real solutions to quadratic equation ax^2 + bx + c = 0, if they exist.
 import math
 a = float(raw_input("Value of a: "))
 if a<> 0:
@@ -8,7 +8,7 @@ if a<> 0:
 	d = b*b-4*a*c
 	if d >=0:
 		if d ==0:
-			print "There is only one solution, namely, ",    (-1*b)/(2*a)
+			print "There is a single (repeated) solution, namely, ",    (-1*b)/(2*a)
 		else:
 			print "The two solutions are",  (-1*b + math.sqrt(d))/(2*a), " and " , (-1*b - math.sqrt(d))/(2*a)
 	else:
